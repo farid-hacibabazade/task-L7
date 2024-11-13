b862868782fe34495c5aba0881d8d05ec11b6fe7
farid-hacibabazade <faridhaci0@gmail.com>
2024-11-13 12:35:19 +0400

    initial commit

---
+++ src/main/java/com/example/task_L7/TaskL7Application.java
@@ -0,0 +1,13 @@
+package com.example.task_L7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskL7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskL7Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/task_L7/TaskL7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.task_L7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskL7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
